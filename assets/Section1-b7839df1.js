946ac8d3060e6806c8352670f6db6ec5d0e2eb8c
CodeKnightly <appevents.bots@gmail.com>
2023-04-06 15:13:41 +0200

    updated router link for pages#2

---
+++ assets/Section1-b7839df1.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,a as c}from"./index-7a2fe89d.js";const s=""+new URL("featurevideo-20241b02.mp4",import.meta.url).href,r={},a={autoplay:"",muted:"",loop:"",id:"FeatureVideo"},n=c("source",{src:s,type:"video/mp4"},null,-1),_=[n];function i(d,p){return o(),t("video",a,_)}const u=e(r,[["render",i]]);export{u as default};
